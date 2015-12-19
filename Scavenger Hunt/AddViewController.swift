c19057949bf8ec30e5ba12fee786271d5fe60a63
Vanesa <vanesa@gmx.net>
2015-12-19 14:17:57 -0800

    add navigation bars

--- Scavenger Hunt/AddViewController.swift
+++ Scavenger Hunt/AddViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AddViewController.swift
 //  Scavenger Hunt
 //
 //  Created by Vanesa Ortiz on 12/19/15.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AddViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func cancel(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
 
