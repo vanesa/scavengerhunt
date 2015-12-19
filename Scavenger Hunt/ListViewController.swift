a1122926c9496d87d0727339c3474b4ebba03768
Vanesa <vanesa@gmx.net>
2015-12-19 12:07:31 -0800

    add text field and table list view

---
+++ Scavenger Hunt/ListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ListViewController.swift
+//  Scavenger Hunt
+//
+//  Created by Vanesa Ortiz on 12/19/15.
+//  Copyright © 2015 Free Spirit Apps. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController : UITableViewController {
+    var itemsList = [ "Cat", "Bird", "Brick"] // each item in the list
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemsList.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ListViewCell", forIndexPath: indexPath)
+        cell.textLabel?.text = itemsList[indexPath.row]
+        return cell
+    }
+}
